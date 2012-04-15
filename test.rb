484c2dab575cc46c4b342e729266241f796242b8
e115763 <e115763@gmail.com>
2012-04-15 22:26:29 +0900

    add test.rb : unit test for AtCoder

---
+++ test.rb
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+require 'stringio'
+require 'test/unit'
+
+# source
+$source = ""
+
+class AtCoder_Test < Test::Unit::TestCase
+
+  def original_test input_str,output_str
+    $stdin = StringIO.new input_str
+    $stdout = StringIO.new
+
+    load $source
+
+    assert_equal(output_str,$stdout.string)
+
+    $stdin = STDIN
+    $stdout.flush
+    $sdtout = STDOUT
+  end
+  
+  # add any test use original_test
+  #
+  # example test code
+  #
+  # def test_1
+  #   input = ""
+  #   output = ""
+  #   original_test input,output
+  # end
+end
